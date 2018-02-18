f74929b04e431f2347be2245076acd7615aaa0dc
xwillis <ximone.willis@utdallas.edu>
2018-02-18 08:57:39 -0700

    delete wear module again probably

---
+++ TestingGauge/mobile/src/main/java/com/example/nutri_000/testinggauge/DisplayScreenArm.java
@@ -0,0 +1,13 @@
+package com.example.nutri_000.testinggauge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayScreenArm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_screen_arm);
+    }
+}
