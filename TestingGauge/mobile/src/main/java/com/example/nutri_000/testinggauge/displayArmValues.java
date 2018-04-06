bf71b8c5772e89af6533a7898fb992665a3afaea
xwillis <ximone.willis@utdallas.edu>
2018-04-06 16:26:17 -0500

    added display files

---
+++ TestingGauge/mobile/src/main/java/com/example/nutri_000/testinggauge/displayArmValues.java
@@ -0,0 +1,13 @@
+package com.example.nutri_000.testinggauge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class displayArmValues extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_arm_values);
+    }
+}
