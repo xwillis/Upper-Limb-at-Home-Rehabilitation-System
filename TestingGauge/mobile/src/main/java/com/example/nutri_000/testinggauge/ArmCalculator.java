e6d1e6cd062d9e238ae6768e342af375e2d5fc3d
xwillis <ximone.willis@utdallas.edu>
2018-04-06 16:24:59 -0500

    arm calculator file updated

--- TestingGauge/mobile/src/main/java/com/example/nutri_000/testinggauge/ArmCalculator.java
+++ TestingGauge/mobile/src/main/java/com/example/nutri_000/testinggauge/ArmCalculator.java
@@ -17,7 +17,7 @@ public class ArmCalculator
     double[] deltaBicepWrist={0,0,0};
     double[] deltaWristHand={0,0,0};
 
-    double[] jointAngles={0,0,0,0,0,0,0};
+    static double[] jointAngles={0,0,0,0,0,0,0};
 
 
     //constructor
@@ -117,42 +117,35 @@ public class ArmCalculator
 			public static double[] thetasForJointAngles(double[] deltaCB, double[] deltaBW, double[] deltaWH)
 
 			{
-                double[][] Matrix =
-                        {
 
-                                //the input values inside the () would access the Xn , Yn , Zn and their respective delta values from the 2nd - 1st sensor
-                                //3rd - 2nd sensor
-                                //4th - 3rd sensor
-                                //will return Matrix (i.e array of array)
-                                { Math.cos(DeltaMatrix[1])*Math.cos(DeltaMatrix[2]) , -Math.cos(DeltaMatrix[1])*Math.sin(DeltaMatrix[2]) , Math.sin(DeltaMatrix[1]) } ,
+                double r112=-Math.cos(deltaCB[1])*Math.sin(deltaCB[2]);
+                double r123=-Math.sin(deltaCB[0])*Math.cos(deltaCB[1]);
+                double r122=-Math.sin(deltaCB[0])*Math.sin(deltaCB[1])*Math.sin(deltaCB[2]) + Math.cos(deltaCB[0])*Math.cos(deltaCB[2]);
+                double r121=Math.sin(deltaCB[0])*Math.sin(deltaCB[1])*Math.cos(deltaCB[2]);
+                double r212=-Math.cos(deltaBW[1])*Math.sin(deltaBW[2]);
+                double r222=-Math.sin(deltaBW[0])*Math.sin(deltaBW[1])*Math.sin(deltaBW[2]) + Math.cos(deltaBW[0])*Math.cos(deltaBW[2]);
+                double r231=-Math.cos(deltaBW[0])*Math.sin(deltaBW[1])*Math.cos(deltaBW[2]) + Math.sin(deltaBW[0])*Math.sin(deltaBW[2]);
+                double r233=Math.cos(deltaBW[0])*Math.cos(deltaBW[1]);
+                double r313=Math.sin(deltaWH[1]);
+                double r323=-Math.sin(deltaWH[0])*Math.cos(deltaWH[1]);
+                double r331=-Math.cos(deltaWH[0])*Math.sin(deltaWH[1])*Math.cos(deltaWH[2]) + Math.sin(deltaWH[0])*Math.sin(deltaWH[2]);
+                double r332=Math.cos(deltaWH[0])*Math.sin(deltaWH[1])*Math.sin(deltaWH[2]) + Math.sin(deltaWH[0])*Math.cos(deltaWH[2]);
 
 
-                                {Math.sin(DeltaMatrix[0])*Math.sin(DeltaMatrix[1])*Math.cos(DeltaMatrix[2]) , -Math.sin(DeltaMatrix[0])*Math.sin(DeltaMatrix[1])*Math.sin(DeltaMatrix[2]) + Math.cos(DeltaMatrix[0])*Math.cos(DeltaMatrix[2]) , -Math.sin(DeltaMatrix[0])*Math.cos(DeltaMatrix[1]) }
-                                ,
-
-                                {-Math.cos(DeltaMatrix[0])*Math.sin(DeltaMatrix[1])*Math.cos(DeltaMatrix[2]) + Math.sin(DeltaMatrix[0])*Math.sin(DeltaMatrix[2]) ,
-
-                                        Math.cos(DeltaMatrix[0])*Math.sin(DeltaMatrix[1])*Math.sin(DeltaMatrix[2]) + Math.sin(DeltaMatrix[0])*Math.cos(DeltaMatrix[2]) ,
-
-                                        Math.cos(DeltaMatrix[0])*Math.cos(DeltaMatrix[1]) }
-
-                        };
-				//creating the index 
-				double [] ThetaValues = new double[7];
-				
 				//using the 3 by 3 matrix but based off of how many elements will need to subtract 1 from each function of theta (i.e based off matrix Ex: r12 or r32) 
 				//in order to produce proper joint value 
-				ThetaValues[0] = Math.atan(-DeltaMatrix[0][1] / DeltaMatrix[2][1]);
-				ThetaValues[1] = Math.asin(DeltaMatrix[1][1]); 
-				ThetaValues[2] = Math.atan(DeltaMatrix[1][0] / -DeltaMatrix[1][2]); 
-				ThetaValues[3] = Math.atan(-DeltaMatrix[0][1] / DeltaMatrix[1][1]);
-				ThetaValues[4] = Math.atan(-DeltaMatrix[2][0] / DeltaMatrix[2][2]);
-				ThetaValues[5] = Math.atan(DeltaMatrix[0][2] / DeltaMatrix[1][2]); 
-				ThetaValues[6] = Math.atan(DeltaMatrix[2][0] / DeltaMatrix[2][1]);
+				jointAngles[0]=Math.atan2(-r112, r123);
+				jointAngles[1]=Math.asin(r122);
+				jointAngles[2]=Math.atan2(r121,-r123);
+				jointAngles[3]=Math.atan2(-r212,r222);
+				jointAngles[4]=Math.atan2(-r231, r233);
+				jointAngles[5]=Math.atan2(r313,r323);
+				jointAngles[6]=Math.atan2(r331,r332);
+
 				
 
 				
-				return ThetaValues; 
+				return jointAngles;
 				
 			}
 			
